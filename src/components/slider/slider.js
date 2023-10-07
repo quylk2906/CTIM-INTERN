0c96ed62acc0fc3fba1fd8884f3dc066cde77986
quylk <quylk@gmail.com>
2023-10-07 16:59:49 +0700

    change document title

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,5 +1,82 @@
+import { useEffect } from 'react';
+import { Helmet } from 'react-helmet';
+import { useState } from 'react';
 import './slider.scss';
 
 export const Slider = () => {
-  return <div className="slider">slider 111</div>;
+  const [docTitle, setDocTitle] = useState('slider');
+  useEffect(() => {
+    var slider = document.querySelector('.slider .list');
+    var items = document.getElementsByClassName('item');
+    var next = document.getElementById('next');
+    var prev = document.getElementById('prev');
+    var dots = document.querySelectorAll('.slider .dots li');
+    var lengthItems = items.length - 1;
+    var active = 0;
+    next.onclick = function () {
+      active = active + 1 <= lengthItems ? active + 1 : 0;
+      setDocTitle(`Slider ${active}`);
+      reloadSlider();
+    };
+    prev.onclick = function () {
+      active = active - 1 >= 0 ? active - 1 : lengthItems;
+      reloadSlider();
+    };
+    var refreshInterval = setInterval(() => {
+      next.click();
+    }, 3000);
+    function reloadSlider() {
+      slider.style.left = -items[active].offsetLeft + 'px';
+      var last_active_dot = document.querySelector('.slider .dots li.active');
+      last_active_dot.classList.remove('active');
+      dots[active].classList.add('active');
+      clearInterval(refreshInterval);
+      refreshInterval = setInterval(() => {
+        next.click();
+      }, 3000);
+    }
+    dots.forEach((li, key) => {
+      li.addEventListener('click', () => {
+        active = key;
+        reloadSlider();
+      });
+    });
+  }, []);
+
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{docTitle}</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+      <div className="slider">
+        <div className="list">
+          <div className="item">
+            <img src="img/1.png" alt="" />
+            <h1 className="text" />
+          </div>
+          <div className="item">
+            <img src="img/2.png" alt="" />
+          </div>
+          <div className="item">
+            <img src="img/3.png" alt="" />
+          </div>
+        </div>
+        <div className="buttons">
+          <span id="prev" className="material-symbols-outlined">
+            arrow_back_ios
+          </span>
+          <span id="next" className="material-symbols-outlined">
+            arrow_forward_ios
+          </span>
+        </div>
+        <ul className="dots">
+          <li className="active" />
+          <li />
+          <li />
+        </ul>
+      </div>
+    </>
+  );
 };
